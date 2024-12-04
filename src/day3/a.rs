65460af84564bab3b6a00275983ebf0178e872bd
DanutAldea <danutz.aldea23@gmail.com>
2024-12-04 23:41:52 +0200

    fixed day3 good practice violtaions

--- src/day3/a.rs
+++ src/day3/a.rs
@@ -5,19 +5,20 @@ fn main() {
 
     let regex = Regex::new(r"mul\(\d{1,3},\d{1,3}\)").unwrap();
     let muls = regex.find_iter(input);
-    
-    let sol: i32 = muls.map(|mul| {
-        mul.as_str()
-            .strip_prefix("mul(")
-            .and_then(|mul| mul.strip_suffix(')'))
-            .and_then(|mul| mul.split_once(','))
-            .and_then(|(x, y)| {
-                match (x.parse::<i32>(), y.parse::<i32>()) {
+
+    let sol: i32 = muls
+        .map(|mul| {
+            mul.as_str()
+                .strip_prefix("mul(")
+                .and_then(|mul| mul.strip_suffix(')'))
+                .and_then(|mul| mul.split_once(','))
+                .and_then(|(x, y)| match (x.parse::<i32>(), y.parse::<i32>()) {
                     (Ok(x), Ok(y)) => Some(x * y),
-                    _ => None
-                }
-            }).expect("Matched string should respect the pattern")
-    }).sum();
+                    _ => None,
+                })
+                .expect("Matched string should respect the pattern")
+        })
+        .sum();
 
     println!("{sol}");
 }
--- src/day3/b.rs
+++ src/day3/b.rs
@@ -1,5 +1,3 @@
-use std::convert::identity;
-
 use regex::Regex;
 
 fn main() {
@@ -9,29 +7,27 @@ fn main() {
     let instructions = regex.find_iter(input);
 
     let mut execute = true;
-    let sol: i32 = instructions.map(|ins| {
-        match ins.as_str() {
+    let sol: i32 = instructions
+        .filter_map(|ins| match ins.as_str() {
             "don't()" => {
                 execute = false;
                 None
-            },
+            }
             "do()" => {
                 execute = true;
                 None
             }
-            mul if execute =>
-                mul.strip_prefix("mul(")
-                    .and_then(|mul| mul.strip_suffix(')'))
-                    .and_then(|mul| mul.split_once(','))
-                    .and_then(|(x, y)| {
-                        match (x.parse::<i32>(), y.parse::<i32>()) {
-                            (Ok(x), Ok(y)) => Some(x * y),
-                            _ => None
-                        }
-                    }),
+            mul if execute => mul
+                .strip_prefix("mul(")
+                .and_then(|mul| mul.strip_suffix(')'))
+                .and_then(|mul| mul.split_once(','))
+                .and_then(|(x, y)| match (x.parse::<i32>(), y.parse::<i32>()) {
+                    (Ok(x), Ok(y)) => Some(x * y),
+                    _ => None,
+                }),
             _ => None,
-        }
-    }).filter_map(identity).sum();
+        })
+        .sum();
 
     println!("{sol}");
 }
